cd45bdfcde257b80150a9b239fdfbc85ab528c13
dontBotherMyName <10bparth@gmail.com>
2020-05-03 21:47:28 +0530

    Assignment 1

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,43 +1,74 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import { DISHES } from '../shared/dishes';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { DISHES } from '../shared/dishes'
 
-class Dishdetail extends Component {
-    constructor(props){
+class DishDetail extends Component {
+    constructor(props) {
         super(props);
         this.state = {
-            
+            dishes: DISHES,
         }
     }
-    
-    renderDish = (dish) =>  {
-
-        if (dish != null) {
+    renderDish(dish) {
+        if(dish == undefined){
             return (
-                <Card>
+                <div></div>
+            )
+        }
+        else {
+            return (
+                <Card>        
                     <CardImg width="100%" src={ dish.image } alt={ dish.name } />
                     <CardBody>
                         <CardTitle> { dish.name } </CardTitle>
                         <CardText> { dish.description } </CardText>
                     </CardBody>
                 </Card>
-            )
+        )
         }
-
-        else {
+    }
+    renderComments(dish) {
+        if(dish == undefined){
             return (
                 <div></div>
             )
         }
+        else {
+            //console.log()
+            let commentOfDish = dish.comments.map((dishName) => {
+                return (
+                    <div key={ dishName.id } className="mt-3">
+                        <ul className="list-unstyled">
+                            <li className="mt-4">{ dishName.comment}</li>
+                            <li className="mt-4">--{ dishName.author }, { dishName.date } </li>
+                        </ul>
+                    </div>
+                )
+            })
+            return (
+                <div>
+                    <h4>Comments</h4>
+                    { commentOfDish }
+                </div>  
+            )
+        }
     }
-    
     render() {
+        let dish = this.props.selectedDish;
+        
         return (
-            <div>
-                
-            </div>
+            <React.Fragment>
+                <div className="col-12 col-md-5 m-1">
+                    {this.renderDish(dish)}
+                            
+                </div>
+                <div className="col-12 col-sm-5 m-1">
+                    {this.renderComments(dish)}
+                </div>
+            </React.Fragment>
+            
         )
     }
 }
 
-export default Dishdetail;
+export default DishDetail;
