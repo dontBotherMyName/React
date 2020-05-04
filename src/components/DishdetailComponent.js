755933ce1241160a5311ca07b5acdfd0bfb2633e
dontBotherMyName <10bparth@gmail.com>
2020-05-04 15:11:18 +0530

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-import { DISHES } from '../shared/dishes'
 
 class DishDetail extends Component {
     renderDish(dish) {
@@ -28,13 +27,12 @@ class DishDetail extends Component {
             )
         }
         else {
-            //console.log()
             let commentOfDish = dish.comments.map((dishName) => {
                 return (
                     <div key={ dishName.id } className="mt-3">
                         <ul className="list-unstyled">
-                            <li className="mt-4">{ dishName.comment}</li>
-                            <li className="mt-4">--{ dishName.author }, { dishName.date } </li>
+                            <li className="mt-4">{ dishName.comment }</li>
+                            <li className="mt-4">--{ dishName.author }, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month:'short', day: '2-digit'}).format(new Date(Date.parse(dishName.date)))}</li>
                         </ul>
                     </div>
                 )
@@ -51,16 +49,16 @@ class DishDetail extends Component {
         let dish = this.props.selectedDish;
         
         return (
-            <React.Fragment>
-                <div className="col-12 col-md-5 m-1">
-                    {this.renderDish(dish)}
-                            
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(dish)}
+                        </div>
+                        <div className="col-12 col-sm-5 m-1">
+                            {this.renderComments(dish)}
+                        </div>
+                    </div>
                 </div>
-                <div className="col-12 col-sm-5 m-1">
-                    {this.renderComments(dish)}
-                </div>
-            </React.Fragment>
-            
         )
     }
 }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -3,22 +3,12 @@ import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 're
 import DishDetail from './DishdetailComponent';
 
 class Menu extends Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedDish: null,
-        }
-    }
-
-    onDishSelect(dish) {
-        this.setState({ selectedDish: dish })
-    }
     
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={ dish.id } className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" src={ dish.image } alt={ dish.name } />
                         <CardImgOverlay>    
                             <CardTitle> { dish.name } </CardTitle>
@@ -32,9 +22,6 @@ class Menu extends Component{
             <div className="container">
                 <div className="row">
                     { menu }
-                </div>
-                <div className="row">
-                    <DishDetail selectedDish={this.state.selectedDish} />
                 </div>
             </div>
         );
